6e8cc628180dcf096ae03765bc80af7cbde29dca
HMMalek <hasna.m.malek@gmail.com>
2020-02-10 01:04:28 +0100

    Updates

---
+++ precache-manifest.262f3c55d2f1a75594573bc568ca07cc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea8c96bbd23c859b4b6403dd1f9d3b0",
+    "url": "/WeatherApp/index.html"
+  },
+  {
+    "revision": "bb12c2b2508ac10b83d8",
+    "url": "/WeatherApp/static/css/2.7f75bbe2.chunk.css"
+  },
+  {
+    "revision": "de847dd5c8d4925e8c8a",
+    "url": "/WeatherApp/static/css/main.6ebdaf4b.chunk.css"
+  },
+  {
+    "revision": "bb12c2b2508ac10b83d8",
+    "url": "/WeatherApp/static/js/2.7c8aee96.chunk.js"
+  },
+  {
+    "revision": "7216103496e649969049620210c2224b",
+    "url": "/WeatherApp/static/js/2.7c8aee96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de847dd5c8d4925e8c8a",
+    "url": "/WeatherApp/static/js/main.163e0438.chunk.js"
+  },
+  {
+    "revision": "8bbb3bf8e4a22c0a13c4",
+    "url": "/WeatherApp/static/js/runtime-main.f5a434f0.js"
+  },
+  {
+    "revision": "addeb6e9d8a5c0a8b255c200ce04103c",
+    "url": "/WeatherApp/static/media/clouds.addeb6e9.ico"
+  },
+  {
+    "revision": "89ba78d146013fa26840af2aacaac7fa",
+    "url": "/WeatherApp/static/media/rain.89ba78d1.ico"
+  },
+  {
+    "revision": "01d21945d1b573d740dd2e4a7242616f",
+    "url": "/WeatherApp/static/media/sunny.01d21945.ico"
+  }
+]);
